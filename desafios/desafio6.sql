00b87aa88ccde7701467e52eeed7477e9bb59742
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-06 19:24:29 -0300

    upando pra finalizar o dia

---
+++ desafios/desafio6.sql
@@ -0,0 +1,16 @@
+USE queries_unite;
+CREATE VIEW amount_spent_by_user AS;
+SELECT u.id as user_id, u.full_name AS user_name,
+SUM(tr.value) AS amount
+FROM users u
+INNER JOIN
+travel_packages tr
+ON tr.travel_package_id = p.travel_package_id
+INNER JOIN
+purchases p
+ON u.id = p.id
+ORDER BY 3;
+
+DROP VIEW amount_spent_by_user;
+SELECT * FROM amount_spent_by_user;
+SELECT * FROM travel_packages;
