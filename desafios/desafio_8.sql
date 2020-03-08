d7dc99b9ffddda05cacc42a3a3f380a4a429bc48
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-08 16:06:47 -0300

    DESAFIO 8

---
+++ desafios/desafio_8.sql
@@ -0,0 +1,24 @@
+SELECT
+MIN((
+SELECT price
+FROM queries_unite.travel_packages 
+WHERE queries_unite.travel_packages.travel_package_id = queries_unite.purchases.travel_package_id
+)) AS lowest_revenue,
+
+MAX((SELECT price
+FROM queries_unite.travel_packages 
+WHERE queries_unite.travel_packages.travel_package_id = queries_unite.purchases.travel_package_id
+)) AS biggest_revenue,
+
+AVG((SELECT price
+FROM queries_unite.travel_packages 
+WHERE queries_unite.travel_packages.travel_package_id = queries_unite.purchases.travel_package_id
+)) AS average_revenue,
+
+SUM((SELECT ROUND(price,2)
+FROM queries_unite.travel_packages 
+WHERE queries_unite.travel_packages.travel_package_id = queries_unite.purchases.travel_package_id
+)) AS total_revenue
+FROM queries_unite.purchases;
+
+-- NÃO CONSEGUI O RESULTADO ESPERADO
