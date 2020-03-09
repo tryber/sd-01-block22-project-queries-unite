0267217c4c2e858314b0d3c562778dae51ac08c5
vrramosm <vf.ramos7@gmail.com>
2020-03-09 10:40:03 -0300

    Consertando identaçao

--- queries/desafio9.sql
+++ queries/desafio9.sql
@@ -2,14 +2,14 @@ DELIMITER $$
 
 CREATE PROCEDURE find_travel_packages_by_period(IN start_date_value VARCHAR(20), IN end_date_value VARCHAR(20))
 BEGIN
-  SELECT travel_package_id AS 'id',
-  package_name AS 'name',
-  value AS 'value',
-  start_date AS departure_date,
-  end_date AS arrival_date
-  FROM travel_packages
-  WHERE start_date > start_date_value
-  AND end_date < end_date_value;
+SELECT travel_package_id AS 'id',
+package_name AS 'name',
+value AS 'value',
+start_date AS departure_date,
+end_date AS arrival_date
+FROM travel_packages
+WHERE start_date > start_date_value
+AND end_date < end_date_value;
 END
 
 $$ DELIMITER ;
