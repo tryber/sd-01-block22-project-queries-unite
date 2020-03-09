584acea3a136dd86bdf87205bcb465cb25408d62
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-03-09 11:42:06 -0300

    atualizando desafio 1

--- desafio-=-1.sql
+++ desafio-=-1.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
 CREATE TABLE travel_packages (
   travel_packages_id INT AUTO_INCREMENT PRIMARY KEY,
   package_name VARCHAR(50) NOT NULL,
-  package_price DECIMAL(7,2) NOT NULL,
+  package_price_total DECIMAL(10,2) NOT NULL,
   purchase_count INT NOT NULL DEFAULT 0,
   departure_date DATE NOT NULL,
   return_date DATE NOT NULL
@@ -24,29 +24,24 @@ CREATE TABLE  locations (
   country VARCHAR(30) NOT NULL
   ) ENGINE = InnoDB;
 
-CREATE TABLE purchases (
-  users_id INT NOT NULL,
-  travel_package_id INT NOT NULL,
-  PRIMARY KEY (users_id, travel_package_id),
-  INDEX fk_users_has_travel_packages_travel_packages1_idx (travel_package_id ASC) VISIBLE,
-  INDEX fk_users_has_travel_packages_users_idx (users_id ASC) VISIBLE,
-  CONSTRAINT fk_users_has_travel_packages_users
-    FOREIGN KEY (users_id)
-    REFERENCES users (id),
-  CONSTRAINT fk_users_has_travel_packages_travel_packages1
-    FOREIGN KEY (travel_package_id)
-    REFERENCES travel_packages (travel_packages_id)
+CREATE TABLE travel_packages_locations (
+travel_packages_locations_id INT AUTO_INCREMENT PRIMARY KEY,
+travel_packages_id INT NOT NULL,
+location_id INT NOT NULL,
+FOREIGN KEY (travel_packages_id) 
+REFERENCES travel_packages(travel_packages_id),
+FOREIGN KEY (location_id) 
+REFERENCES locations(location_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE travel_packages_locations (
-  travel_packages_id INT NOT NULL,
-  location_id INT NOT NULL,
-  PRIMARY KEY (travel_packages_id, location_id),
-  FOREIGN KEY (travel_packages_id)
-  REFERENCES travel_packages (travel_packages_id),
-  FOREIGN KEY (location_id)
-  REFERENCES locations (location_id)
-  ) ENGINE = InnoDB;
+CREATE TABLE purchases (
+purchase_id INT AUTO_INCREMENT PRIMARY KEY,
+users_id INT NOT NULL,
+travel_packages_id INT NOT NULL,
+FOREIGN KEY (users_id) REFERENCES users(id),
+FOREIGN KEY (travel_packages_id) REFERENCES travel_packages(travel_packages_id),
+purchase_count INT DEFAULT 0
+) ENGINE = InnoDB;
 
 INSERT INTO users (full_name, current_age, current_job)
 VALUES 
@@ -62,14 +57,14 @@ VALUES
 ('Marrocos', 'Marrakesh'),
 ('França', 'Paris'),
 ('Estados Unidos', 'Nova Iorque');
-INSERT INTO travel_packages (package_name, departure_date, return_date, package_price) 
+INSERT INTO travel_packages (package_name, departure_date, return_date, package_price_total) 
 VALUES
 ('Promoção 3 destinos', '2022/09/25', '2022/10/12', 12500.99),
 ('Vem Pro Verão', '2022/10/16', '2022/10/26', 9650.99),
 ('Cultura Francesa', '2022/07/12', '2022/07/20', 3900.99),
 ('Vem Pro Verão', '2022/02/19', '2022/03/26', 7725.99),
 ('Promoção 3 destinos', '2022/09/13', '2022/10/18', 13500.99);
-INSERT INTO purchases (users_id, travel_package_id) 
+INSERT INTO purchases (users_id, travel_packages_id) 
 VALUES
 (1, 1),
 (2, 2),
@@ -77,6 +72,6 @@ VALUES
 (4, 4),
 (5, 3),
 (5, 5);
-INSERT INTO travel_packages_locations (travel_package_id, location_id) 
+INSERT INTO travel_packages_locations (travel_packages_id, location_id) 
 VALUES
 (1, 1), (1, 2), (1, 3), (2, 4), (2, 5), (3, 4), (4, 1), (4, 2), (5, 3), (5, 5), (5, 4);
