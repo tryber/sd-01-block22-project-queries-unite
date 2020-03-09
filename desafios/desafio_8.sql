9dff94f97022a5ca0b80297d96ab5746a1e089e0
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-09 11:51:00 -0300

    Cc

--- desafios/desafio_8.sql
+++ desafios/desafio_8.sql
@@ -22,5 +22,3 @@ FROM queries_unite.travel_packages
 WHERE queries_unite.travel_packages.travel_package_id = queries_unite.purchases.travel_package_id
 )) AS total_revenue
 FROM queries_unite.purchases;
-
--- NÃO CONSEGUI O RESULTADO ESPERADO
