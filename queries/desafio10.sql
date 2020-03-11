f3b72a7bd80a5acd992942fe5b5c39368909f38a
vrramosm <vf.ramos7@gmail.com>
2020-03-11 09:24:50 -0300

    Adicionado o resultado esperado dentro da variavel average_amout. Modificado o tipo do retorno de DOUBLE para INT

--- queries/desafio10.sql
+++ queries/desafio10.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION average_amount_spent_given_age_range(min_age INT, max_age INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE average_amout DOUBLE;
+DECLARE average_amout INT;
 SELECT ROUND(AVG(TR.value), 2)
 FROM purchases AS PU
 INNER JOIN travel_packages AS TR
@@ -11,7 +11,7 @@ ON TR.travel_package_id = PU.travel_package_id
 INNER JOIN users AS US
 ON US.id = PU.id
 WHERE US.current_age BETWEEN (min_age)
-AND (max_age);
+AND (max_age) INTO average_amout;
 RETURN average_amout;
 END
 
