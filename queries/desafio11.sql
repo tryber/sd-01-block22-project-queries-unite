dd754756e3d94a14694be4ccf5662f54f32f7df4
vrramosm <vf.ramos7@gmail.com>
2020-03-11 09:42:28 -0300

    Consertado o desafio 11

--- queries/desafio11.sql
+++ queries/desafio11.sql
@@ -1,12 +1,16 @@
 DELIMITER $$
 
 CREATE TRIGGER increment_travel_package_purchases
-AFTER INSERT ON purchases
+BEFORE INSERT ON purchases
 FOR EACH ROW
 BEGIN
-UPDATE travel_packages AS TR
+UPDATE travel_packages
 SET TR.purchase_count = (purchase_count + 1)
 WHERE TR.travel_package_id = NEW.travel_package_id;
 END 
 
 $$ DELIMITER ;
+
+SELECT travel_package_id AS id,
+purchase_count 
+FROM queries_unite.travel_packages;
