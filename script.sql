54c3109770f6e408a2af9a6b445aaaa318597907
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-03-11 09:33:08 -0300

    desafio 5

--- script.sql
+++ script.sql
@@ -7,13 +7,41 @@
 -- Travel Package 1 : N Destination
 -- Travel Package (purchase_count default 0)
 -- Primary Key auto_increment (Except Bridge Table)
---
+-- 
+
+
+
+
+
+-- Rafael Martins user_name
+-- 33 user_age
+-- job_title
+
+
+-- 'Promoção 3 destinos' travel_packages
+
+
+-- R$12.500,99 package_price
+
+
+-- 25/09/2020 departure_date
+-- 12/10/2022 return_date
+-- locations country
+-- travel_packages_locations
+-- purchases
+
+ 
+
+
+
 
 -- Nome; Idade; Profissão;
 -- Nome do Pacote; Destino: Cidade; País; Data de Ida; Data de Volta; Custo Total; 
 
 
--- IDENTIFICAR ENTIDADES, ATRIBUTOS E RELACIONAMENTOS
+-- Identify entities, attributes and relationships
+-- Identify Entities: 
+
 
 -- popular o banco de dados
 
